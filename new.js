b7995d769b71a999944c33f834b33294529dc962
ROMAN <roma0660123@gmail.com>
2024-12-03 23:52:29 +0500

    first commit

---
+++ new.js
@@ -0,0 +1,157 @@
+//VARIABLES
+
+// let name = 'John'; //String
+// let age = 25; //Number
+// let isMarried = false; //Boolean
+// let children = ['Anna', 'Peter']; //Array
+// let person = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false,
+//     children: ['Anna', 'Peter']
+// }
+// isMarried = true;
+// person.isMarried = true;
+// const height = 180; //Constant
+
+
+
+
+//CONDITIONALS
+// let height = 170;
+
+// if(height >= 180) {
+//     console.log('you are tall');
+// }
+// else if(height < 180 && height >= 170) {
+//     console.log('average')
+// }
+// else {
+//     console.log('short')
+// }
+
+
+// let age = 18;
+
+// if(age >= 18) {
+//     console.log('adult');
+// }
+// else {
+//     console.log('young')
+// }
+
+
+//LOOPS
+
+// let human = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false,
+//     children: ['Anna', 'Peter', 'Nick']
+// }
+
+// // console.log(human.name)
+// console.log('age ' + human.age)
+
+// for(let i = 0; i < human.children.length; i++) {
+//     console.log(human.children[i]);
+// }
+
+// const phones = [
+//     {
+//         brand: 'Apple',
+//         model: 'iphone 12',
+//         price: 1000,
+//         storage: [128, 256,512]
+//     },
+//     {
+//         brand: 'Samsung',
+//         model: 'Gallaxy S21',
+//         price: 900,
+//         storage: [128, 256,512]
+//     },
+//     {
+//         brand: 'Google',
+//         model: 'Pixel 5',
+//         price: 700,
+//         storage: [64, 128]
+//     }
+// ]
+
+
+// for(let i = 0; i < phones.length; i++) {
+//     console.log('brand ' + phones[i].brand);
+//     console.log('models ' + phones[i].model);
+//     console.log('price ' + phones[i].price);
+//     for(let i2 = 0; i2 < phones[i].storage.length; i2++) {
+//         console.log('storage GB ' + phones[i].storage[i2]);
+//     }
+// }
+
+
+// let fruits = [
+//     {
+//         name: 'Apple',
+//         location: ['USA', 'Canada', 'UK'],
+//     },
+//     {
+//         name: 'Banana',
+//         location: ['USA', 'Mexico'],
+//     },
+//     {
+//         name: 'Orange',
+//         location: ['Uzbekistan', 'Brazil'],
+//     },
+// ];
+
+// for(let i = 0; i < fruits.length; i++) {
+//     console.log('name - ' + fruits[i].name)
+//     for(let i2 = 0; i2 < fruits[i].location.length; i2++) {
+//         console.log('location - ' + fruits[i].location[i2]);
+//     }
+// }
+
+// for(let i = 0; i < fruits.length; i++) {
+//     console.log('Fruits ' + fruits[i].name)
+//     for(let i2 = 0; i2 < fruits[i].location.length; i2++) {
+//         console.log('Location ' + fruits[i].location[i2])
+//     }
+
+// }
+
+
+//FUNCTIONS
+
+
+// let num1 = 10;
+// let num2 = 20;
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// add(10, 20)
+
+// let n1 = 1;
+// let n2 = 1;
+// let n3 = 1;
+// function add(n1, n2, n3) {
+//     console.log(n1 + n2 + n3);
+// }
+// add(n1, n2, n3)
+
+// function displayArray(array) {
+//     for(let i = 0; i < array.length; i++) {
+//         console.log('- ' + array[i]);
+//     }
+// }
+
+
+// let cars = ['BMW', 'Audi', 'Mercedes'];
+// let countries = ['USA', 'Canada', 'Mexico'];
+// let animals = ['Dog', 'Cat', 'Bird'];
+
+
+// displayArray(animals);
+// displayArray(cars);
+// displayArray(countries);
+
